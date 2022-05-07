2c9a963631fab0a9aaf81884fef4de4aebca3c7a
Nadia Nikolaeva <44nikolaeva@mail.ru>
2022-05-07 12:18:47 +0300

    renamed numpy functions

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import sympy as sp
 import random
+import sympy as sp
 import numpy as np
 from sympy.abc import z
 import functions as mpf
-import numpy as geek
 
 # диапозон варьирования "истиных" значений коэффициентов модели 
 num = 5.0
@@ -19,7 +18,7 @@ observations_num = 7 # N
 deleted_observations_num = 2
 
 # коэффициенты "истинной" модели
-model_coefficient = geek.empty([1, parametrs_num])
+model_coefficient = np.empty([1, parametrs_num])
 for i in range(1):
     for j in range(parametrs_num):
         model_coefficient[i][j] = random.uniform(-num,num)     
@@ -29,7 +28,7 @@ print(model_coefficient)
 # факторы для "истинной" модели
 xx = float(10.0)
 
-matrix_X = geek.empty([observations_num, parametrs_num + 1])
+matrix_X = np.empty([observations_num, parametrs_num + 1])
 
 for i in range(parametrs_num + 1):
         matrix_X[i][0] = 1          
@@ -41,7 +40,7 @@ for i in range(observations_num):
 print(matrix_X)
 
 # "истинные" результаты надюдений (идиальная сходимость)
-transpose_X = geek.empty([parametrs_num + 1, observations_num])
+transpose_X = np.empty([parametrs_num + 1, observations_num])
 
 for i in range(len(matrix_X)):
    for j in range(len(matrix_X[0])):
@@ -51,7 +50,7 @@ matrix_Y0 = [[sum(a * b for a, b in zip(A_row, B_col))
                         for B_col in zip(*transpose_X)]
                                 for A_row in model_coefficient]   
 
-transpose_Y0 = geek.empty([parametrs_num + 1, 1])
+transpose_Y0 = np.empty([parametrs_num + 1, 1])
 
 for i in range(len(matrix_Y0)):
    for j in range(len(matrix_Y0[0])):
@@ -63,12 +62,12 @@ print(transpose_Y0)
 error_coefficient = 0.3
 error_range = random.uniform(-error_coefficient,error_coefficient)
 
-marix_ERR = geek.empty([1, observations_num])
+marix_ERR = np.empty([1, observations_num])
 for i in range(1):
     for j in range(observations_num):
         marix_ERR[i][j] = matrix_Y0[i][j] * random.uniform(-error_coefficient,error_coefficient)     
 
-transpose_ERR = geek.empty([observations_num, 1])
+transpose_ERR = np.empty([observations_num, 1])
 
 for i in range(len(marix_ERR)):
    for j in range(len(marix_ERR[0])):
@@ -82,10 +81,19 @@ matrix_Y = transpose_Y0 + transpose_ERR
 print(matrix_Y)
 
 # значения факторов в "фактических" наблюдениях (после исключения строк)
-new_matrix_X = geek.empty([observations_num - 2, parametrs_num + 1])
+new_matrix_X = np.empty([observations_num - 2, parametrs_num + 1])
 
 for i in range(observations_num - 2):
     for j in range(parametrs_num):
         new_matrix_X[i][j] = matrix_X[i][j]
 
 print(new_matrix_X)
+
+# коэффициенты модели по "факическим" результатам наблюдений
+new_matrix_Y = np.empty([observations_num - 2, 1])
+
+for i in range(observations_num - 2):
+    for j in range(1):
+        new_matrix_Y[i][j] = matrix_Y[i][j]
+
+print(new_matrix_Y)
